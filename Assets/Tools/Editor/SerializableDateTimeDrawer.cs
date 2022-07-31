88ca9029a03cded115ec65db6f236f67455c74ca
JD <arkham195@gmail.com>
2022-07-31 20:03:44 +0200

    serializable date and time

---
+++ Assets/Tools/Editor/SerializableDateTimeDrawer.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(SerializableDateTime))]
+public class SerializableDateTimeDrawer : PropertyDrawer
+{
+	private static GUIContent label2 = new GUIContent("Date");
+	private static GUIContent label1 = new GUIContent("Time");
+
+	private static GUIContent[] subLabels1 = new GUIContent[3]
+	{
+			new GUIContent("Year"),
+			new GUIContent("Month"),
+			new GUIContent("Day"),
+	};
+
+	private static GUIContent[] subLabels2 = new GUIContent[3]{
+			new GUIContent("Hour"),
+			new GUIContent("Minute"),
+			new GUIContent("Second"),
+		};
+
+	private static int[] values1 = new int[3];
+	private static int[] values2 = new int[3];
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return base.GetPropertyHeight(property, label) * 2f + EditorGUIUtility.standardVerticalSpacing;
+	}
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		EditorGUI.BeginProperty(position, label, property);
+		var year = property.FindPropertyRelative("year");
+		var month = property.FindPropertyRelative("month");
+		var day = property.FindPropertyRelative("day");
+		var hour = property.FindPropertyRelative("hour");
+		var minute = property.FindPropertyRelative("minute");
+		var second = property.FindPropertyRelative("second");
+
+		values1[0] = year.intValue;
+		values1[1] = month.intValue;
+		values1[2] = day.intValue;
+		values2[0] = hour.intValue;
+		values2[1] = minute.intValue;
+		values2[2] = second.intValue;
+
+		position.height *= 0.5f;
+
+		var orgPos = position;
+
+		position = EditorGUI.PrefixLabel(position, label2);
+		EditorGUI.MultiIntField(position, subLabels1, values1);
+
+		position = orgPos;
+
+		position.y += position.height + EditorGUIUtility.standardVerticalSpacing;
+		position = EditorGUI.PrefixLabel(position, label1);
+		EditorGUI.MultiIntField(position, subLabels2, values2);
+
+		year.intValue = values1[0];
+		month.intValue = values1[1];
+		day.intValue = values1[2];
+		hour.intValue = values2[0];
+		minute.intValue = values2[1];
+		second.intValue = values2[2];
+
+		EditorGUI.EndProperty();
+	}
+}
---
+++ Assets/Tools/Helper/SerializableDateTime.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct SerializableDateTime
+{
+	[SerializeField] private int year;
+	[SerializeField] private int month;
+	[SerializeField] private int day;
+	[SerializeField] private int hour;
+	[SerializeField] private int minute;
+	[SerializeField] private int second;
+
+	public TimeSpan GetFromDate(DateTime date2)
+	{
+		return this - date2;
+	}
+
+	public TimeSpan GetFromNow()
+	{
+		return GetFromDate(DateTime.Now);
+	}
+
+	public TimeSpan GetFromToday()
+	{
+		return GetFromDate(DateTime.Today);
+	}
+
+	public static implicit operator DateTime(SerializableDateTime date) => new DateTime(date.year, date.month, date.day, date.hour, date.minute, date.second);
+}
