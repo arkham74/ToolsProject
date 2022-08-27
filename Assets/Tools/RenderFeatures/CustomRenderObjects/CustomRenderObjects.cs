994f1ef03056a556a7e244836375319fa3d1b8d0
JD <arkham195@gmail.com>
2022-08-27 21:51:41 +0200

    extend built-in render feature to use rendering layers

---
+++ Assets/Tools/RenderFeatures/CustomRenderObjects/CustomRenderObjects.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+
+namespace JD.CustomRenderObjects
+{
+	[ExcludeFromPreset]
+	[Tooltip("Render Objects simplifies the injection of additional render passes by exposing a selection of commonly used settings.")]
+	public class CustomRenderObjects : ScriptableRendererFeature
+	{
+		[Serializable]
+		public class Settings
+		{
+			public string passTag = "CustomRenderObjectsFeature";
+			public RenderPassEvent @event = RenderPassEvent.AfterRenderingOpaques;
+			public FilterSettings filterSettings = new FilterSettings();
+			public Material overrideMaterial;
+			public int overrideMaterialPassIndex;
+			public bool overrideDepthState;
+			public CompareFunction depthCompareFunction = CompareFunction.LessEqual;
+			public bool enableWrite = true;
+			public StencilStateData stencilSettings = new StencilStateData();
+			public CustomCameraSettings cameraSettings = new CustomCameraSettings();
+		}
+
+		[Serializable]
+		public class FilterSettings
+		{
+			public RenderQueueType renderQueueType;
+			public LayerMask layerMask;
+			public uint renderLayerMask;
+			public string[] passNames;
+
+			public FilterSettings()
+			{
+				renderQueueType = RenderQueueType.Opaque;
+				layerMask = -1;
+				renderLayerMask = 1;
+			}
+		}
+
+		[Serializable]
+		public class CustomCameraSettings
+		{
+			public bool overrideCamera;
+			public bool restoreCamera = true;
+			public Vector4 offset;
+			public float cameraFieldOfView = 60.0f;
+		}
+
+		public Settings settings = new Settings();
+
+		private CustomRenderObjectsPass renderObjectsPass;
+
+		public override void Create()
+		{
+			FilterSettings filter = settings.filterSettings;
+
+			// Render Objects pass doesn't support events before rendering pre-passes.
+			// The camera is not setup before this point and all rendering is monoscopeic.
+			// Events before BeforeRenderingPre-passes should be used for input texture passes (shadow map, LUT, etc) that doesn't depend on the camera.
+			// These events are filtering in the UI, but we still should prevent users from changing it from code or
+			// by changing the serialized data.
+			if (settings.@event < RenderPassEvent.BeforeRenderingPrePasses)
+				settings.@event = RenderPassEvent.BeforeRenderingPrePasses;
+
+			renderObjectsPass = new CustomRenderObjectsPass(settings.passTag, settings.@event, filter.passNames,
+				filter.renderQueueType, filter.layerMask, filter.renderLayerMask, settings.cameraSettings);
+
+			renderObjectsPass.OverrideMaterial = settings.overrideMaterial;
+			renderObjectsPass.OverrideMaterialPassIndex = settings.overrideMaterialPassIndex;
+
+			if (settings.overrideDepthState)
+				renderObjectsPass.SetDetphState(settings.enableWrite, settings.depthCompareFunction);
+
+			if (settings.stencilSettings.overrideStencilState)
+				renderObjectsPass.SetStencilState(settings.stencilSettings.stencilReference,
+					settings.stencilSettings.stencilCompareFunction, settings.stencilSettings.passOperation,
+					settings.stencilSettings.failOperation, settings.stencilSettings.zFailOperation);
+		}
+
+		public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+		{
+			renderer.EnqueuePass(renderObjectsPass);
+		}
+
+		internal override bool SupportsNativeRenderPass()
+		{
+			return true;
+		}
+	}
+}
---
+++ Assets/Tools/RenderFeatures/CustomRenderObjects/CustomRenderObjectsPass.cs
@@ -0,0 +1,145 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine.Scripting.APIUpdating;
+
+namespace JD.CustomRenderObjects
+{
+	public class CustomRenderObjectsPass : ScriptableRenderPass
+	{
+		private readonly RenderQueueType renderQueueType;
+		private FilteringSettings mFilteringSettings;
+		private readonly CustomRenderObjects.CustomCameraSettings mCameraSettings;
+		private readonly ProfilingSampler mProfilingSampler;
+
+		public Material OverrideMaterial { get; set; }
+		public int OverrideMaterialPassIndex { get; set; }
+
+		private readonly List<ShaderTagId> mShaderTagIdList = new List<ShaderTagId>();
+
+		public void SetDetphState(bool writeEnabled, CompareFunction function = CompareFunction.Less)
+		{
+			mRenderStateBlock.mask |= RenderStateMask.Depth;
+			mRenderStateBlock.depthState = new DepthState(writeEnabled, function);
+		}
+
+		public void SetStencilState(int reference, CompareFunction compareFunction, StencilOp passOp, StencilOp failOp, StencilOp zFailOp)
+		{
+			StencilState stencilState = StencilState.defaultValue;
+			stencilState.enabled = true;
+			stencilState.SetCompareFunction(compareFunction);
+			stencilState.SetPassOperation(passOp);
+			stencilState.SetFailOperation(failOp);
+			stencilState.SetZFailOperation(zFailOp);
+
+			mRenderStateBlock.mask |= RenderStateMask.Stencil;
+			mRenderStateBlock.stencilReference = reference;
+			mRenderStateBlock.stencilState = stencilState;
+		}
+
+		private RenderStateBlock mRenderStateBlock;
+
+		public CustomRenderObjectsPass(string profilerTag, RenderPassEvent renderPassEvent, string[] shaderTags, RenderQueueType renderQueueType, int layerMask, uint renderLayerMask, CustomRenderObjects.CustomCameraSettings cameraSettings)
+		{
+			profilingSampler = new ProfilingSampler(nameof(CustomRenderObjectsPass));
+
+			mProfilingSampler = new ProfilingSampler(profilerTag);
+			this.renderPassEvent = renderPassEvent;
+			this.renderQueueType = renderQueueType;
+			OverrideMaterial = null;
+			OverrideMaterialPassIndex = 0;
+			RenderQueueRange renderQueueRange = (renderQueueType == RenderQueueType.Transparent)
+				? RenderQueueRange.transparent
+				: RenderQueueRange.opaque;
+			mFilteringSettings = new FilteringSettings(renderQueueRange, layerMask, renderLayerMask);
+
+			if (shaderTags is { Length: > 0 })
+			{
+				foreach (string passName in shaderTags)
+					mShaderTagIdList.Add(new ShaderTagId(passName));
+			}
+			else
+			{
+				mShaderTagIdList.Add(new ShaderTagId("SRPDefaultUnlit"));
+				mShaderTagIdList.Add(new ShaderTagId("UniversalForward"));
+				mShaderTagIdList.Add(new ShaderTagId("UniversalForwardOnly"));
+			}
+
+			mRenderStateBlock = new RenderStateBlock(RenderStateMask.Nothing);
+			mCameraSettings = cameraSettings;
+		}
+
+		internal CustomRenderObjectsPass(URPProfileId profileId, RenderPassEvent renderPassEvent, string[] shaderTags, RenderQueueType renderQueueType, int layerMask, uint renderLayerMask, CustomRenderObjects.CustomCameraSettings cameraSettings) : this(profileId.GetType().Name, renderPassEvent, shaderTags, renderQueueType, layerMask, renderLayerMask, cameraSettings)
+		{
+			mProfilingSampler = ProfilingSampler.Get(profileId);
+		}
+
+		public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+		{
+			SortingCriteria sortingCriteria = (renderQueueType == RenderQueueType.Transparent) ? SortingCriteria.CommonTransparent : renderingData.cameraData.defaultOpaqueSortFlags;
+
+			DrawingSettings drawingSettings = CreateDrawingSettings(mShaderTagIdList, ref renderingData, sortingCriteria);
+			drawingSettings.overrideMaterial = OverrideMaterial;
+			drawingSettings.overrideMaterialPassIndex = OverrideMaterialPassIndex;
+
+			ref CameraData cameraData = ref renderingData.cameraData;
+			Camera camera = cameraData.camera;
+
+			// In case of camera stacking we need to take the viewport rect from base camera
+			Rect pixelRect = renderingData.cameraData.pixelRect;
+			float cameraAspect = pixelRect.width / pixelRect.height;
+
+			// NOTE: Do NOT mix ProfilingScope with named CommandBuffers i.e. CommandBufferPool.Get("name").
+			// Currently there's an issue which results in mismatched markers.
+			CommandBuffer cmd = CommandBufferPool.Get();
+			using (new ProfilingScope(cmd, mProfilingSampler))
+			{
+				if (mCameraSettings.overrideCamera)
+				{
+					if (cameraData.xr.enabled)
+					{
+						Debug.LogWarning("CustomRenderObjects pass is configured to override camera matrices. While rendering in stereo camera matrices cannot be overridden.");
+					}
+					else
+					{
+						Matrix4x4 projectionMatrix = Matrix4x4.Perspective(mCameraSettings.cameraFieldOfView, cameraAspect,
+							camera.nearClipPlane, camera.farClipPlane);
+						projectionMatrix = GL.GetGPUProjectionMatrix(projectionMatrix, cameraData.IsCameraProjectionMatrixFlipped());
+
+						Matrix4x4 viewMatrix = cameraData.GetViewMatrix();
+						Vector4 cameraTranslation = viewMatrix.GetColumn(3);
+						viewMatrix.SetColumn(3, cameraTranslation + mCameraSettings.offset);
+
+						RenderingUtils.SetViewAndProjectionMatrices(cmd, viewMatrix, projectionMatrix, false);
+					}
+				}
+
+				DebugHandler activeDebugHandler = GetActiveDebugHandler(renderingData);
+				if (activeDebugHandler != null)
+				{
+					activeDebugHandler.DrawWithDebugRenderState(context, cmd, ref renderingData, ref drawingSettings, ref mFilteringSettings, ref mRenderStateBlock,
+						(ScriptableRenderContext ctx, ref RenderingData data, ref DrawingSettings ds, ref FilteringSettings fs, ref RenderStateBlock rsb) => { ctx.DrawRenderers(data.cullResults, ref ds, ref fs, ref rsb); });
+				}
+				else
+				{
+					// Ensure we flush our command-buffer before we render...
+					context.ExecuteCommandBuffer(cmd);
+					cmd.Clear();
+
+					// Render the objects...
+					context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref mFilteringSettings, ref mRenderStateBlock);
+				}
+
+				if (mCameraSettings.overrideCamera && mCameraSettings.restoreCamera && !cameraData.xr.enabled)
+				{
+					RenderingUtils.SetViewAndProjectionMatrices(cmd, cameraData.GetViewMatrix(), cameraData.GetGPUProjectionMatrix(), false);
+				}
+			}
+
+			context.ExecuteCommandBuffer(cmd);
+			CommandBufferPool.Release(cmd);
+		}
+	}
+}
