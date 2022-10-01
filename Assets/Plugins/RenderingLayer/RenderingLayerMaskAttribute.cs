0d257a072913a0dc91462f3e5daf1373d38e3e6c
JD <arkham195@gmail.com>
2022-10-01 15:00:51 +0200

    remove

--- Assets/Plugins/RenderingLayer/RenderingLayerMaskAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-public class RenderingLayerMaskAttribute : PropertyAttribute
-{
-	public RenderingLayerMaskAttribute() { }
-}
