00e7a91783d675edadaba29a1de50bd25198d6ad
JD <arkham195@gmail.com>
2022-10-01 17:10:14 +0200

    new hex

--- Assets/Tools/Helper/Hex.cs
+++ Assets/Tools/Helper/Hex.cs
@@ -1,151 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using TMPro;
 using Freya;
-using UnityEngine;
+using Random = UnityEngine.Random;
+using Text = TMPro.TextMeshProUGUI;
+using Tools = JD.Tools;
+using System.Runtime.CompilerServices;
 
 namespace JD
 {
-	public static class Hex
+	public readonly partial struct Hex : IEquatable<Hex>, IFormattable
 	{
-		private const float SQRT3 = 1.732050807568877293527446341505872366942805253810380628055806f;
+		public readonly float q;
+		public readonly float r;
+		public readonly float s;
 
-		public static Vector2 HexToWorld(Vector2 hex, float radius)
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Hex(float q, float r, float s)
 		{
-			return HexToWorld(hex, Vector2.one * radius);
+			this.q = q;
+			this.r = r;
+			this.s = s;
 		}
 
-		public static Vector2 HexToWorld(Vector2 hex, Vector2 radius)
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Hex(float q, float r)
 		{
-			return HexToWorld((Vector3)hex, radius);
+			this.q = q;
+			this.r = r;
+			this.s = -q - r;
 		}
 
-		public static Vector2 HexToWorld(Vector3 hex, float radius)
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex operator -(Hex hex) => hex.Negate();
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex operator +(Hex a, Hex b) => a.Add(b);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex operator -(Hex a, Hex b) => a.Sub(b);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool operator ==(Hex a, Hex b) => a.Equals(b);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool operator !=(Hex a, Hex b) => !a.Equals(b);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override int GetHashCode() => q.GetHashCode() ^ (r.GetHashCode() << 2) ^ (s.GetHashCode() >> 2);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override string ToString() => ToString(null, null);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public string ToString(string format, IFormatProvider formatProvider) => $"({q}, {r}, {s})";
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Equals(Hex other) => q == other.q && r == other.r && s == other.s;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public override bool Equals(object obj)
 		{
-			return HexToWorld(hex, Vector2.one * radius);
-		}
-
-		public static Vector2 HexToWorld(Vector3 hex, Vector2 radius)
-		{
-			float x = radius.x * (SQRT3 * hex.x + SQRT3 / 2f * hex.y);
-			float y = radius.y * (3f / 2f * hex.y);
-			return new Vector2(x, y);
-		}
-
-		public static Vector3 WorldToHex(Vector2 point, float radius)
-		{
-			return WorldToHex(point, Vector2.one * radius);
-		}
-
-		public static Vector3 WorldToHex(Vector2 point, Vector2 radius)
-		{
-			float q = SQRT3 / 3f * (point.x / radius.x) - 1f / 3f * (point.y / radius.y);
-			float r = 2f / 3f * (point.y / radius.y);
-			return new Vector3(q, r, -q - r);
-		}
-
-		public static Vector2Int HexToOffset(Vector2Int hex)
-		{
-			return HexToOffset((Vector3Int)hex);
-		}
-
-		public static Vector2Int HexToOffset(Vector3Int hex)
-		{
-			int col = hex.x + (hex.y + (hex.y & 1)) / 2;
-			int row = hex.y;
-			return new Vector2Int(col, row);
-		}
-
-		public static Vector2Int OffsetToHex(Vector2Int hex)
-		{
-			return OffsetToHex((Vector3Int)hex);
-		}
-
-		public static Vector2Int OffsetToHex(Vector3Int hex)
-		{
-			int q = hex.x - (hex.y + (hex.y & 1)) / 2;
-			int r = hex.y;
-			return new Vector2Int(q, r);
-		}
-
-		public static Vector3Int RoundToInt(Vector2 hex)
-		{
-			return Round(CalcS(hex)).RoundToInt();
-		}
-
-		public static Vector3Int RoundToInt(Vector3 hex)
-		{
-			return Round(hex).RoundToInt();
-		}
-
-		public static Vector3 Round(Vector3 hex)
-		{
-			float q = Mathf.Round(hex.x);
-			float r = Mathf.Round(hex.y);
-			float s = Mathf.Round(hex.z);
-
-			float qDiff = Mathf.Abs(q - hex.x);
-			float rDiff = Mathf.Abs(r - hex.y);
-			float sDiff = Mathf.Abs(s - hex.z);
-
-			if (qDiff > rDiff && qDiff > sDiff)
+			if (obj is Hex hex)
 			{
-				q = -r - s;
-			}
-			else if (rDiff > sDiff)
-			{
-				r = -q - s;
-			}
-			else
-			{
-				s = -q - r;
+				return Equals(hex);
 			}
 
-			return new Vector3(q, r, s);
-		}
-
-		public static Vector3Int[] GetNeighboursHex(Vector3Int start)
-		{
-			return new Vector3Int[6]
-			{
-				CalcS(new Vector2Int(1, 0)) + start,
-				CalcS(new Vector2Int(-1, 0)) + start,
-				CalcS(new Vector2Int(0, 1)) + start,
-				CalcS(new Vector2Int(0, -1)) + start,
-				CalcS(new Vector2Int(1, -1)) + start,
-				CalcS(new Vector2Int(-1, 1)) + start,
-			};
-		}
-
-		public static float Distance(Vector2 start, Vector2 end)
-		{
-			return Distance(CalcS(start), CalcS(end));
-		}
-
-		public static float Distance(Vector3 start, Vector3 end)
-		{
-			Vector3 vec = start - end;
-			float x = Mathf.Abs(vec.x);
-			float y = Mathf.Abs(vec.y);
-			float z = Mathf.Abs(vec.z);
-			return (x + y + z) / 2f;
-		}
-
-		public static Vector3Int CalcS(Vector3Int hex)
-		{
-			return CalcS((Vector3)hex).RoundToInt();
-		}
-
-		public static Vector3Int CalcS(Vector2Int hex)
-		{
-			return CalcS((Vector2)hex).RoundToInt();
-		}
-
-		public static Vector3 CalcS(Vector2 hex)
-		{
-			Vector3 cube = (Vector3)hex;
-			cube.z = -hex.x - hex.y;
-			return cube;
+			return false;
 		}
 	}
 }
---
+++ Assets/Tools/Helper/HexUtils.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace JD
+{
+	public static class HexUtils
+	{
+		private const float SQRT3 = 1.732050807568877293527446341505872366942805253810380628055806f;
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromWorld(this Vector3 point) => FromWorld((Vector2)point);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromAxial(this Vector2 qr) => FromAxial(qr.x, qr.y);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromCube(this Vector3 qrs) => FromCube(qrs.x, qrs.y, qrs.z);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromOffset(this Vector2Int xy) => FromOffset(xy.x, xy.y);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromAxial(float q, float r) => new Hex(q, r, -q - r);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromCube(float q, float r, float s) => new Hex(q, r, s);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector2 ToAxial(this Hex hex) => new Vector2(hex.q, hex.r);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector3 ToCube(this Hex hex) => new Vector3(hex.q, hex.r, hex.s);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector2 ToWorld(this Hex hex)
+		{
+			float x = SQRT3 * hex.q + SQRT3 / 2f * hex.r;
+			float y = 3f / 2f * hex.r;
+			return new Vector2(x, y);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromWorld(this Vector2 point)
+		{
+			float y = point.y;
+			float x = point.x;
+			float q = SQRT3 / 3f * x - 1f / 3f * y;
+			float r = 2f / 3f * y;
+			return new Hex(q, r, -q - r);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex[] Neighbours(this Hex hex)
+		{
+			return new Hex[6]
+			{
+				Add(new Hex(1, 0),hex),
+				Add(new Hex(-1, 0),hex),
+				Add(new Hex(0, 1),hex),
+				Add(new Hex(0, -1),hex),
+				Add(new Hex(1, -1),hex),
+				Add(new Hex(-1, 1),hex),
+			};
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float Distance(this Hex a, Hex b)
+		{
+			Hex vec = Sub(a, b);
+			float q = Mathf.Abs(vec.q);
+			float r = Mathf.Abs(vec.r);
+			float s = Mathf.Abs(vec.s);
+			return (q + r + s) / 2f;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex Negate(this Hex hex)
+		{
+			float q = -hex.q;
+			float r = -hex.r;
+			float s = -q - r;
+			return new Hex(q, r, s);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex Sub(this Hex a, Hex b)
+		{
+			float q = a.q - b.q;
+			float r = a.r - b.r;
+			float s = a.s - b.s;
+			return new Hex(q, r, s);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex Add(this Hex a, Hex b)
+		{
+			float q = a.q + b.q;
+			float r = a.r + b.r;
+			float s = a.s + b.s;
+			return new Hex(q, r, s);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex FromOffset(int x, int y)
+		{
+			int q = x - (y + (y & 1)) / 2;
+			int r = y;
+			return FromAxial(q, r);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector2 ToOffset(this Hex hex)
+		{
+			Hex rounded = hex.Round();
+			int q = (int)rounded.q;
+			int r = (int)rounded.r;
+			int col = q + (r + (r & 1)) / 2;
+			int row = r;
+			return new Vector2Int(col, row);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Hex Round(this Hex hex)
+		{
+			float q = Mathf.Round(hex.q);
+			float r = Mathf.Round(hex.r);
+			float s = Mathf.Round(hex.s);
+
+			float qDiff = Mathf.Abs(q - hex.q);
+			float rDiff = Mathf.Abs(r - hex.r);
+			float sDiff = Mathf.Abs(s - hex.s);
+
+			if (qDiff > rDiff && qDiff > sDiff)
+			{
+				q = -r - s;
+			}
+			else if (rDiff > sDiff)
+			{
+				r = -q - s;
+			}
+			else
+			{
+				s = -q - r;
+			}
+
+			return new Hex(q, r, s);
+		}
+	}
+}
