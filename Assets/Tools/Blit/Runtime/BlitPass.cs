c8a8d94205bdf96abe2cf77c7636c9f721afa23b
JD <arkham195@gmail.com>
2022-10-23 12:17:28 +0200

    Update BlitPass.cs

--- Assets/Tools/Blit/Runtime/BlitPass.cs
+++ Assets/Tools/Blit/Runtime/BlitPass.cs
@@ -34,89 +34,85 @@ namespace Cyan
 
 		public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
 		{
-			if (!sceneView && renderingData.cameraData.camera.name == "SceneCamera")
+			if (sceneView || !renderingData.cameraData.isSceneViewCamera)
 			{
-				return;
-			}
-
-			if (settings.requireDepthNormals)
-			{
-				ConfigureInput(ScriptableRenderPassInput.Normal);
+				if (settings.requireDepthNormals)
+				{
+					ConfigureInput(ScriptableRenderPassInput.Normal);
+				}
 			}
 		}
 
 		public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
 		{
-			if (!sceneView && renderingData.cameraData.camera.name == "SceneCamera")
+			if (sceneView || !renderingData.cameraData.isSceneViewCamera)
 			{
-				return;
-			}
+				CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+				RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+				opaqueDesc.depthBufferBits = 0;
 
-			CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
-			RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
-			opaqueDesc.depthBufferBits = 0;
+				// Set Source / Destination
+				var renderer = renderingData.cameraData.renderer;
 
-			// Set Source / Destination
-			var renderer = renderingData.cameraData.renderer;
-
-			// note : Seems this has to be done in here rather than in AddRenderPasses to work correctly in 2021.2+
-			if (settings.srcType == Target.CameraColor)
-			{
-				source = renderer.cameraColorTarget;
-			}
-			else if (settings.srcType == Target.TextureID)
-			{
-				source = new RenderTargetIdentifier(settings.srcTextureId);
-			}
-			else if (settings.srcType == Target.RenderTextureObject)
-			{
-				source = new RenderTargetIdentifier(settings.srcTextureObject);
-			}
-
-			if (settings.dstType == Target.CameraColor)
-			{
-				destination = renderer.cameraColorTarget;
-			}
-			else if (settings.dstType == Target.TextureID)
-			{
-				destination = new RenderTargetIdentifier(settings.dstTextureId);
-			}
-			else if (settings.dstType == Target.RenderTextureObject)
-			{
-				destination = new RenderTargetIdentifier(settings.dstTextureObject);
-			}
-
-			if (settings.setInverseViewMatrix)
-			{
-				Shader.SetGlobalMatrix("_InverseView", renderingData.cameraData.camera.cameraToWorldMatrix);
-			}
-
-			if (settings.dstType == Target.TextureID)
-			{
-				if (settings.overrideGraphicsFormat)
+				// note : Seems this has to be done in here rather than in AddRenderPasses to work correctly in 2021.2+
+				if (settings.srcType == Target.CameraColor)
 				{
-					opaqueDesc.graphicsFormat = settings.graphicsFormat;
+					source = renderer.cameraColorTarget;
 				}
-				cmd.GetTemporaryRT(m_DestinationTexture.id, opaqueDesc, filterMode);
-			}
+				else if (settings.srcType == Target.TextureID)
+				{
+					source = new RenderTargetIdentifier(settings.srcTextureId);
+				}
+				else if (settings.srcType == Target.RenderTextureObject)
+				{
+					source = new RenderTargetIdentifier(settings.srcTextureObject);
+				}
 
-			Shader.SetGlobalFloat("_UnscaledTime", Time.unscaledTime);
+				if (settings.dstType == Target.CameraColor)
+				{
+					destination = renderer.cameraColorTarget;
+				}
+				else if (settings.dstType == Target.TextureID)
+				{
+					destination = new RenderTargetIdentifier(settings.dstTextureId);
+				}
+				else if (settings.dstType == Target.RenderTextureObject)
+				{
+					destination = new RenderTargetIdentifier(settings.dstTextureObject);
+				}
 
-			//Debug.Log($"src = {source},     dst = {destination} ");
-			// Can't read and write to same color target, use a TemporaryRT
-			if (source == destination || (settings.srcType == settings.dstType && settings.srcType == Target.CameraColor))
-			{
-				cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, filterMode);
-				Blit(cmd, source, m_TemporaryColorTexture.Identifier(), blitMaterial, settings.blitMaterialPassIndex);
-				Blit(cmd, m_TemporaryColorTexture.Identifier(), destination);
-			}
-			else
-			{
-				Blit(cmd, source, destination, blitMaterial, settings.blitMaterialPassIndex);
-			}
+				if (settings.setInverseViewMatrix)
+				{
+					Shader.SetGlobalMatrix("_InverseView", renderingData.cameraData.camera.cameraToWorldMatrix);
+				}
 
-			context.ExecuteCommandBuffer(cmd);
-			CommandBufferPool.Release(cmd);
+				if (settings.dstType == Target.TextureID)
+				{
+					if (settings.overrideGraphicsFormat)
+					{
+						opaqueDesc.graphicsFormat = settings.graphicsFormat;
+					}
+					cmd.GetTemporaryRT(m_DestinationTexture.id, opaqueDesc, filterMode);
+				}
+
+				Shader.SetGlobalFloat("_UnscaledTime", Time.unscaledTime);
+
+				//Debug.Log($"src = {source},     dst = {destination} ");
+				// Can't read and write to same color target, use a TemporaryRT
+				if (source == destination || (settings.srcType == settings.dstType && settings.srcType == Target.CameraColor))
+				{
+					cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, filterMode);
+					Blit(cmd, source, m_TemporaryColorTexture.Identifier(), blitMaterial, settings.blitMaterialPassIndex);
+					Blit(cmd, m_TemporaryColorTexture.Identifier(), destination);
+				}
+				else
+				{
+					Blit(cmd, source, destination, blitMaterial, settings.blitMaterialPassIndex);
+				}
+
+				context.ExecuteCommandBuffer(cmd);
+				CommandBufferPool.Release(cmd);
+			}
 		}
 
 		public override void OnCameraCleanup(CommandBuffer cmd)
