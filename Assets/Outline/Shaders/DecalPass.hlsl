ab5c3dc3e5a43f862060661b3e1401b9c4bf6371
JD <arkham195@gmail.com>
2023-03-16 19:42:06 +0100

    remove

--- Assets/Outline/Shaders/DecalPass.hlsl
+++
@@ -1,9 +0,0 @@
-TEXTURE2D(_DBufferTexture0);
-SAMPLER(sampler_DBufferTexture0);
-
-float4 frag (v2f i) : SV_Target
-{
-	float4 decal = _DBufferTexture0.Sample(sampler_DBufferTexture0, i.uv);
-	decal.a = 1 - decal.a;
-	return decal;
-}
