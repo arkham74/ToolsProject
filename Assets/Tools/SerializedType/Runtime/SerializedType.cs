2b1608c52a8a5361f64481e4e211af33b67e2f2a
Jason Davies <arkham195@gmail.com>
2023-03-23 17:50:36 +0100

    Update SerializedType.cs

--- Assets/Tools/SerializedType/Runtime/SerializedType.cs
+++ Assets/Tools/SerializedType/Runtime/SerializedType.cs
@@ -30,10 +30,10 @@ namespace JD
 			return v1;
 		}
 
-		// public static implicit operator SerializedType<T>(string fullName) => new SerializedType<T>(fullName);
-		// public static implicit operator string(SerializedType<T> serializedType) => serializedType.fullName;
+		public static implicit operator SerializedType<T>(string fullName) => new SerializedType<T>(fullName);
+		public static implicit operator string(SerializedType<T> serializedType) => serializedType.fullName;
 
-		// public static implicit operator Type(SerializedType<T> serializedType) => Type.GetType(serializedType);
-		// public static implicit operator SerializedType<T>(Type type) => new SerializedType<T>(type.FullName);
+		public static implicit operator Type(SerializedType<T> serializedType) => Type.GetType(serializedType);
+		public static implicit operator SerializedType<T>(Type type) => new SerializedType<T>(type.FullName);
 	}
 }
