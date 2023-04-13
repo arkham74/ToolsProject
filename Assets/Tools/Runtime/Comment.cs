f97145bcaf476aec3f10d97b5c8535a3990b2a09
Jason Davies <arkham195@gmail.com>
2023-04-13 15:57:52 +0200

    comment component

---
+++ Assets/Tools/Runtime/Comment.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using TMPro;
+using JD;
+using Freya;
+using Random = UnityEngine.Random;
+using Text = TMPro.TextMeshProUGUI;
+using Tools = JD.Tools;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace JD
+{
+	public class Comment : MonoBehaviour
+	{
+		[SerializeField][TextArea] private string comment;
+	}
+}
