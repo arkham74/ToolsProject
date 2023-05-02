445f9cbca43711041a79a74c9b561413b05d2f64
Jason Davies <arkham195@gmail.com>
2023-05-02 14:39:27 +0200

    add button extensions

--- Assets/Tools/Extensions/Runtime/ButtonExtensions.cs
+++ Assets/Tools/Extensions/Runtime/ButtonExtensions.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
@@ -5,6 +8,20 @@ namespace JD
 {
 	public static class ButtonExtensions
 	{
+		public static IEnumerator WaitForClick(this Button button)
+		{
+			bool click = false;
+			void Click() => click = true;
+			button.onClick.AddListener(Click);
+			yield return new WaitUntil(() => click);
+			button.onClick.RemoveListener(Click);
+		}
+
+		public static void RemoveAllListeners(this Button button)
+		{
+			button.onClick.RemoveAllListeners();
+		}
+
 		public static void ReplaceListener(this Button button, UnityAction func)
 		{
 			button.onClick.RemoveAllListeners();
