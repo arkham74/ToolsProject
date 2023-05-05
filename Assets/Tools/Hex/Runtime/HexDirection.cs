5016b2c0ebeae1e985c767f5232772ea161a8c85
Jason Davies <arkham195@gmail.com>
2023-05-05 16:59:02 +0200

    hex direction aliases

--- Assets/Tools/Hex/Runtime/HexDirection.cs
+++ Assets/Tools/Hex/Runtime/HexDirection.cs
@@ -6,11 +6,17 @@ namespace JD
 	public enum HexDirection
 	{
 		Right = 1,
+		East = 1,
 		DownRight = 2,
+		SouthEast = 2,
 		DownLeft = 4,
+		SouthWest = 4,
 		Left = 8,
+		West = 8,
 		UpLeft = 16,
+		NorthWest = 16,
 		UpRight = 32,
+		NorthEast = 32,
 		All = Right | DownRight | DownLeft | Left | UpLeft | UpRight,
 	}
 }
