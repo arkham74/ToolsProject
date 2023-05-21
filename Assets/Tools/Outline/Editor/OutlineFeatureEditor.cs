686ce8aea5c6f8002590c036c50594a9d9a6f150
Jason Davies <arkham195@gmail.com>
2023-05-22 00:11:02 +0200

    custom feature editor

---
+++ Assets/Tools/Outline/Editor/OutlineFeatureEditor.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using JD;
+using Random = UnityEngine.Random;
+using UnityEditor;
+using UnityEditorInternal;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace JD.Outline.Editor
+{
+	[CustomEditor(typeof(OutlineFeature))]
+	public class OutlineFeatureEditor : UnityEditor.Editor
+	{
+		private SerializedProperty settingsProp;
+
+		private void OnEnable()
+		{
+			settingsProp = serializedObject.FindProperty("settings");
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+			if (GUILayout.Button("Add outline settings"))
+			{
+				settingsProp.InsertArrayElementAtIndex(settingsProp.arraySize);
+			}
+			for (int i = 0; i < settingsProp.arraySize; i++)
+			{
+				SerializedProperty property = settingsProp.GetArrayElementAtIndex(i);
+				EditorGUILayout.BeginHorizontal();
+				EditorGUILayout.PropertyField(property, GUIContent.none);
+				if (GUILayout.Button("Remove outline settings"))
+				{
+					settingsProp.DeleteArrayElementAtIndex(i);
+				}
+				EditorGUILayout.EndHorizontal();
+			}
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
