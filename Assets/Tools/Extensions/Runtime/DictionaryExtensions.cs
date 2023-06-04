28a0141d6c999182c7e5e13a26f68fe940523510
Jason Davies <arkham195@gmail.com>
2023-06-04 13:32:03 +0200

    rename

--- Assets/Tools/Extensions/Runtime/DictionaryExtensions.cs
+++ Assets/Tools/Extensions/Runtime/DictionaryExtensions.cs
@@ -4,14 +4,14 @@ namespace JD
 {
 	public static class DictionaryExtensions
 	{
-		public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+		public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue add)
 		{
 			if (dictionary.TryGetValue(key, out TValue value))
 			{
 				return value;
 			}
-			dictionary.Add(key, defaultValue);
-			return defaultValue;
+			dictionary.Add(key, add);
+			return add;
 		}
 	}
 }
