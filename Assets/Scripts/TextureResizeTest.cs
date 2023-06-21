e2c9137705384c657de674484930fc2471435a08
Jason Davies <arkham195@gmail.com>
2023-06-21 23:05:55 +0200

    resize with filter

---
+++ Assets/Scripts/TextureResizeTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using TMPro;
+using JD;
+using Freya;
+using Random = UnityEngine.Random;
+using Text = TMPro.TextMeshProUGUI;
+using Tools = JD.Tools;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+public class TextureResizeTest : MonoBehaviour
+{
+	[SerializeField] private FilterMode filterMode = FilterMode.Bilinear;
+	[SerializeField] private int width = 64;
+	[SerializeField] private int height = 64;
+	[SerializeField] private RawImage input;
+	[SerializeField] private RawImage output;
+
+	private void OnValidate()
+	{
+		Resize();
+	}
+
+	private void Resize()
+	{
+		Texture2D texture = (Texture2D)input.texture;
+		if (filterMode == FilterMode.Point)
+		{
+			texture = texture.ResizePoint(width, height);
+		}
+		else
+		{
+			texture = texture.ResizeBilinear(width, height);
+		}
+		texture.filterMode = FilterMode.Point;
+		output.texture = texture;
+	}
+}
---
+++ Assets/Tools/Extensions/Runtime/TextureExtensions.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+namespace JD
+{
+	public static class TextureExtensions
+	{
+		public static Texture2D ResizePointNonAlloc(this Texture2D source, int width, int height)
+		{
+			RenderTextureDescriptor descriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 0);
+			RenderTexture renderTexture = RenderTexture.GetTemporary(descriptor);
+			Graphics.Blit(source, renderTexture);
+			source.Reinitialize(width, height);
+			renderTexture.CopyTo(source);
+			RenderTexture.ReleaseTemporary(renderTexture);
+			return source;
+		}
+
+		public static Texture2D ResizeBilinearNonAlloc(this Texture2D source, int width, int height)
+		{
+			RenderTextureDescriptor smallDescriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 0);
+			RenderTextureDescriptor bigDescriptor = new RenderTextureDescriptor(source.width, source.height, RenderTextureFormat.ARGB32, 0);
+			bigDescriptor.useMipMap = true;
+			bigDescriptor.autoGenerateMips = true;
+			RenderTexture smallRenderTexture = RenderTexture.GetTemporary(smallDescriptor);
+			RenderTexture bigRenderTexture = RenderTexture.GetTemporary(bigDescriptor);
+			Graphics.Blit(source, bigRenderTexture);
+			Graphics.Blit(bigRenderTexture, smallRenderTexture);
+			source.Reinitialize(width, height);
+			smallRenderTexture.CopyTo(source);
+			RenderTexture.ReleaseTemporary(smallRenderTexture);
+			RenderTexture.ReleaseTemporary(bigRenderTexture);
+			return source;
+		}
+
+		public static Texture2D ResizePoint(this Texture2D source, int width, int height)
+		{
+			RenderTextureDescriptor descriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 0);
+			RenderTexture renderTexture = RenderTexture.GetTemporary(descriptor);
+			Graphics.Blit(source, renderTexture);
+			Texture2D destination = renderTexture.ToTexture2D();
+			RenderTexture.ReleaseTemporary(renderTexture);
+			return destination;
+		}
+
+		public static Texture2D ResizeBilinear(this Texture2D source, int width, int height)
+		{
+			RenderTextureDescriptor smallDescriptor = new RenderTextureDescriptor(width, height, RenderTextureFormat.ARGB32, 0);
+			RenderTextureDescriptor bigDescriptor = new RenderTextureDescriptor(source.width, source.height, RenderTextureFormat.ARGB32, 0);
+			bigDescriptor.useMipMap = true;
+			bigDescriptor.autoGenerateMips = true;
+			RenderTexture smallRenderTexture = RenderTexture.GetTemporary(smallDescriptor);
+			RenderTexture bigRenderTexture = RenderTexture.GetTemporary(bigDescriptor);
+			Graphics.Blit(source, bigRenderTexture);
+			Graphics.Blit(bigRenderTexture, smallRenderTexture);
+			Texture2D destination = smallRenderTexture.ToTexture2D();
+			RenderTexture.ReleaseTemporary(smallRenderTexture);
+			RenderTexture.ReleaseTemporary(bigRenderTexture);
+			return destination;
+		}
+
+		public static Texture2D ToTexture2D(this RenderTexture source)
+		{
+			int mips = source.mipmapCount;
+			TextureCreationFlags flags = mips > 1 ? TextureCreationFlags.MipChain : TextureCreationFlags.None;
+			Texture2D dest = new Texture2D(source.width, source.height, source.graphicsFormat, mips, flags);
+			source.CopyTo(dest);
+			return dest;
+		}
+
+		public static RenderTexture ToRenderTexture(this Texture2D source)
+		{
+			RenderTexture dest = new RenderTexture(source.width, source.height, 0, DefaultFormat.HDR);
+			dest.enableRandomWrite = true;
+			source.BlitTo(dest);
+			return dest;
+		}
+
+		public static void BlitTo(this Texture2D source, RenderTexture dest)
+		{
+			Graphics.Blit(source, dest);
+		}
+
+		public static void CopyTo(this RenderTexture source, Texture2D dest)
+		{
+			RenderTexture prev = RenderTexture.active;
+			RenderTexture.active = source;
+			dest.ReadPixels(new Rect(0, 0, dest.width, dest.height), 0, 0, source.useMipMap);
+			dest.Apply();
+			RenderTexture.active = prev;
+		}
+
+		public static void CopyTo(this Texture2D source, RenderTexture dest)
+		{
+			Graphics.CopyTexture(source, dest);
+		}
+
+		public static void Clear(this RenderTexture texture, bool clearDepth, bool clearColor, Color backgroundColor, float depth = 1.0f)
+		{
+			RenderTexture prev = RenderTexture.active;
+			RenderTexture.active = texture;
+			GL.Clear(clearDepth, clearColor, backgroundColor, depth);
+			RenderTexture.active = prev;
+		}
+
+		public static void ClearWithSkybox(this RenderTexture texture, bool clearDepth)
+		{
+			texture.ClearWithSkybox(clearDepth, Camera.main);
+		}
+
+		public static void ClearWithSkybox(this RenderTexture texture, bool clearDepth, Camera camera)
+		{
+			RenderTexture prev = RenderTexture.active;
+			RenderTexture.active = texture;
+			GL.ClearWithSkybox(clearDepth, camera);
+			RenderTexture.active = prev;
+		}
+	}
+}
