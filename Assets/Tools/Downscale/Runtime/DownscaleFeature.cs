fb73c88db0793937b824db447318b19cfdfb7eb2
Jason Davies <arkham195@gmail.com>
2023-06-22 21:39:22 +0200

    rewrite downscale feature

--- Assets/Tools/Downscale/Runtime/DownscaleFeature.cs
+++ Assets/Tools/Downscale/Runtime/DownscaleFeature.cs
@@ -1,36 +1,27 @@
+#if TOOLS_URP
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
-
-#if TOOLS_URP
 using UnityEngine.Rendering.Universal;
+
 namespace JD
 {
 	public partial class DownscaleFeature : ScriptableRendererFeature
 	{
-		private DownscalePass scriptablePass;
-		public int targetRes = 180;
-		// public Vector2Int targetRes = new Vector2Int(320, 180);
-		// public GraphicsFormat graphicsFormat = GraphicsFormat.R8G8B8A8_SRGB;
-		public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+		[SerializeField][Min(1)] private int targetRes = 45;
+		[SerializeField] private bool bilinear = false;
+		[SerializeField] private RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
 
-		/// <inheritdoc/>
+		private DownscalePass scriptablePass;
+
 		public override void Create()
 		{
-			scriptablePass = new DownscalePass
-			{
-				profilerTag = name,
-				targetRes = targetRes,
-				// graphicsFormat = graphicsFormat,
-				renderPassEvent = renderPassEvent
-			};
+			scriptablePass = new DownscalePass(name, targetRes, bilinear);
 		}
 
-		// Here you can inject one or multiple render passes in the renderer.
-		// This method is called when setting up the renderer once per-camera.
 		public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
 		{
-			scriptablePass.source = renderer.cameraColorTarget;
+			scriptablePass.renderPassEvent = renderPassEvent;
 			renderer.EnqueuePass(scriptablePass);
 		}
 	}
--- Assets/Tools/Downscale/Runtime/DownscalePass.cs
+++ Assets/Tools/Downscale/Runtime/DownscalePass.cs
@@ -10,75 +10,66 @@ namespace JD
 	{
 		private class DownscalePass : ScriptableRenderPass
 		{
-			public string profilerTag;
-			public int targetRes = 180;
-			// public GraphicsFormat graphicsFormat = GraphicsFormat.R8G8B8A8_SRGB;
-			public RenderTargetIdentifier source;
-			// private RenderTargetIdentifier downTarget;
-			private RenderTargetIdentifier upTarget;
-			// private int downTargetId;
-			private int upTargetId;
+			private static readonly int bigBufferId = Shader.PropertyToID("_BigBuffer");
+			private static readonly int smallBufferId = Shader.PropertyToID("_SmallBuffer");
 
-			// This method is called before executing the render pass.
-			// It can be used to configure render targets and their clear state. Also to create temporary render target textures.
-			// When empty this render pass will render to the active camera render target.
-			// You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
-			// The render pipeline will ensure target setup and clearing happens in a performant manner.
+			private string profilerTag;
+			private int targetRes;
+			private bool bilinear;
+
+			public DownscalePass(string profilerTag, int targetRes)
+			{
+				this.profilerTag = profilerTag;
+				this.targetRes = targetRes;
+			}
+
+			public DownscalePass(string profilerTag, int targetRes, bool bilinear) : this(profilerTag, targetRes)
+			{
+				this.bilinear = bilinear;
+			}
+
 			public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
 			{
-				RenderTextureDescriptor upDescriptor = renderingData.cameraData.cameraTargetDescriptor;
-				// RenderTextureDescriptor downDescriptor = renderingData.cameraData.cameraTargetDescriptor;
-				// float aspect = (float)downDescriptor.width / (float)downDescriptor.height;
-				// downDescriptor.width = Mathf.FloorToInt(aspect * targetRes.y);
-				// downDescriptor.width = targetRes.x;
-				// Debug.LogWarning(downDescriptor.width);
-				// downDescriptor.height = targetRes.y;
+				CameraData cameraData = renderingData.cameraData;
 
-				// upDescriptor.graphicsFormat = graphicsFormat;
-				// downDescriptor.graphicsFormat = graphicsFormat;
+				RenderTextureDescriptor bigDescriptor = cameraData.cameraTargetDescriptor;
+				bigDescriptor.autoGenerateMips = true;
+				bigDescriptor.useMipMap = true;
 
-				// downTargetId = Shader.PropertyToID("downScaleTarget");
-				upTargetId = Shader.PropertyToID("upScaleTarget");
+				cmd.GetTemporaryRT(bigBufferId, bigDescriptor, FilterMode.Trilinear);
 
-				cmd.GetTemporaryRT(upTargetId, upDescriptor, FilterMode.Point);
-				// cmd.GetTemporaryRT(downTargetId, downDescriptor, FilterMode.Point);
+				RenderTextureDescriptor smallDescriptor = cameraData.cameraTargetDescriptor;
+				smallDescriptor.width = (int)(targetRes * cameraData.camera.aspect);
+				smallDescriptor.height = targetRes;
 
-				// downTarget = new RenderTargetIdentifier(downTargetId);
-				upTarget = new RenderTargetIdentifier(upTargetId);
-
-				// ConfigureTarget(downTarget);
-				ConfigureTarget(upTarget);
+				cmd.GetTemporaryRT(smallBufferId, smallDescriptor, FilterMode.Point);
 			}
 
-			// Here you can implement the rendering logic.
-			// Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
-			// https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
-			// You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
 			public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
 			{
-				int height = renderingData.cameraData.cameraTargetDescriptor.height;
+				RenderTargetIdentifier colorTarget = renderingData.cameraData.renderer.cameraColorTarget;
+				CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
 
-				if (height % targetRes == 0)
+				if (bilinear)
 				{
-					CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
-
-					int mult = height / targetRes;
-
-					cmd.Blit(source, upTarget, Vector2.one * mult, Vector2.zero);
-					cmd.Blit(upTarget, source, Vector2.one * (1f / mult), Vector2.zero);
-
-					context.ExecuteCommandBuffer(cmd);
-					cmd.Clear();
-					CommandBufferPool.Release(cmd);
+					Blit(cmd, colorTarget, bigBufferId);
+					Blit(cmd, bigBufferId, smallBufferId);
+					Blit(cmd, smallBufferId, colorTarget);
 				}
+				else
+				{
+					Blit(cmd, colorTarget, smallBufferId);
+					Blit(cmd, smallBufferId, colorTarget);
+				}
+
+				context.ExecuteCommandBuffer(cmd);
+				CommandBufferPool.Release(cmd);
 			}
 
-			// Cleanup any allocated resources that were created during the execution of this render pass.
 			public override void OnCameraCleanup(CommandBuffer cmd)
 			{
-				// cmd.ReleaseTemporaryRT(downTargetId);
-				cmd.ReleaseTemporaryRT(upTargetId);
-				ConfigureClear(ClearFlag.All, Color.black);
+				cmd.ReleaseTemporaryRT(bigBufferId);
+				cmd.ReleaseTemporaryRT(smallBufferId);
 			}
 		}
 	}
