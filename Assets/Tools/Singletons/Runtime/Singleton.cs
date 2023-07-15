5ae8f0744f62f8ea2eb6210c55ac383681e3c484
Jason Davies <arkham195@gmail.com>
2023-07-15 13:51:14 +0200

    hide error log in validation

--- Assets/Tools/Singletons/Runtime/Singleton.cs
+++ Assets/Tools/Singletons/Runtime/Singleton.cs
@@ -6,29 +6,42 @@ namespace JD
 	public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 	{
 		private static T instance;
-		public static bool Valid => Instance != null;
+
+		public static bool Valid
+		{
+			get
+			{
+				Load(true);
+				return instance;
+			}
+		}
 
 		public static T Instance
 		{
 			get
 			{
-				if (instance == null)
+				Load();
+				return instance;
+			}
+		}
+
+		private static void Load(bool silent = false)
+		{
+			if (instance == null)
+			{
+				T[] type = FindObjectsOfType<T>(true);
+				if (type.Length > 0)
 				{
-					T[] type = FindObjectsOfType<T>(true);
-					if (type.Length > 0)
+					instance = type[0];
+					if (type.Length > 1)
 					{
-						instance = type[0];
-						if (type.Length > 1)
-						{
-							Debug.LogError($"There is more than one '{typeof(T)}' in scene");
-						}
-					}
-					else
-					{
-						Debug.LogError($"There is no '{typeof(T)}' present in scene");
+						Debug.LogError($"There is more than one '{typeof(T)}' in scene");
 					}
 				}
-				return instance;
+				else if (!silent)
+				{
+					Debug.LogError($"There is no '{typeof(T)}' present in scene");
+				}
 			}
 		}
 
