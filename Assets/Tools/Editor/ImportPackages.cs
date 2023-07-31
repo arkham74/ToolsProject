34c943e2082c32c2e659ea82e44fd940a9071486
Jason Davies <arkham195@gmail.com>
2023-07-31 14:37:52 +0200

    ad sg vars

--- Assets/Tools/Editor/ImportPackages.cs
+++ Assets/Tools/Editor/ImportPackages.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEditor;
 using UnityEditor.PackageManager;
+using UnityEngine;
 
 namespace JD.Editor
 {
@@ -9,26 +11,35 @@ namespace JD.Editor
 		static ImportPackages()
 		{
 #if !TOOLS_TOOLBAR
-			Client.Add("https://github.com/marijnz/unity-toolbar-extender.git");
+			ClientAdd("https://github.com/marijnz/unity-toolbar-extender.git");
 #endif
 #if !TOOLS_FREYA
-			Client.Add("https://github.com/FreyaHolmer/Mathfs.git");
+			ClientAdd("https://github.com/FreyaHolmer/Mathfs.git");
 #endif
 #if !TOOLS_COMP_VIS
-			Client.Add("com.needle.compilation-visualizer");
+			ClientAdd("com.needle.compilation-visualizer");
 #endif
 #if !TOOLS_RENAMER
-			Client.Add("https://github.com/redbluegames/unity-mulligan-renamer.git?path=/Assets/RedBlueGames/MulliganRenamer");
+			ClientAdd("https://github.com/redbluegames/unity-mulligan-renamer.git?path=/Assets/RedBlueGames/MulliganRenamer");
 #endif
 #if !TOOLS_NAUATTR
-			Client.Add("com.dbrizov.naughtyattributes");
+			ClientAdd("com.dbrizov.naughtyattributes");
 #endif
 #if !TOOLS_ANIM_SEQ
-			Client.Add("com.brunomikoski.animationsequencer");
+			ClientAdd("com.brunomikoski.animationsequencer");
 #endif
 #if !TOOLS_TWEEN_PLAYABLES
-			Client.Add("https://github.com/AnnulusGames/TweenPlayables.git?path=/Assets/TweenPlayables");
+			ClientAdd("https://github.com/AnnulusGames/TweenPlayables.git?path=/Assets/TweenPlayables");
 #endif
+#if !TOOLS_SG_VARS
+			ClientAdd("https://github.com/Cyanilux/ShaderGraphVariables.git");
+#endif
+		}
+
+		private static void ClientAdd(string link)
+		{
+			Debug.LogWarning(link);
+			Client.Add(link);
 		}
 	}
 }
