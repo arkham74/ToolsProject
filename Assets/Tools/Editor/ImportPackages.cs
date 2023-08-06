917d90f8d5cf3950cea96e509db4924b54b49511
Jason Davies <arkham195@gmail.com>
2023-08-06 14:14:47 +0200

    tweaknames

--- Assets/Tools/Editor/ImportPackages.cs
+++ Assets/Tools/Editor/ImportPackages.cs
@@ -1,46 +1,46 @@
-using System;
-using UnityEditor;
-using UnityEditor.PackageManager;
-using UnityEngine;
+// using System;
+// using UnityEditor;
+// using UnityEditor.PackageManager;
+// using UnityEngine;
 
-namespace JD.Editor
-{
-	public class ImportPackages
-	{
-		[InitializeOnLoadMethod]
-		private static void AddPackages()
-		{
-			// Debug.LogWarning("InitializeOnLoadMethod");
-#if !TOOLS_TOOLBAR
-			ClientAdd("https://github.com/marijnz/unity-toolbar-extender.git");
-#endif
-#if !TOOLS_FREYA
-			ClientAdd("https://github.com/FreyaHolmer/Mathfs.git");
-#endif
-#if !TOOLS_COMP_VIS
-			ClientAdd("com.needle.compilation-visualizer");
-#endif
-#if !TOOLS_RENAMER
-			ClientAdd("https://github.com/redbluegames/unity-mulligan-renamer.git?path=/Assets/RedBlueGames/MulliganRenamer");
-#endif
-#if !TOOLS_NAUATTR
-			ClientAdd("com.dbrizov.naughtyattributes");
-#endif
-#if !TOOLS_ANIM_SEQ
-			ClientAdd("com.brunomikoski.animationsequencer");
-#endif
-#if !TOOLS_TWEEN_PLAYABLES
-			ClientAdd("https://github.com/AnnulusGames/TweenPlayables.git?path=/Assets/TweenPlayables");
-#endif
-#if !TOOLS_SG_VARS
-			ClientAdd("https://github.com/Cyanilux/ShaderGraphVariables.git");
-#endif
-		}
+// namespace JD.Editor
+// {
+// 	public class ImportPackages
+// 	{
+// 		[InitializeOnLoadMethod]
+// 		private static void AddPackages()
+// 		{
+// 			// Debug.LogWarning("InitializeOnLoadMethod");
+// #if !TOOLS_TOOLBAR
+// 			ClientAdd("https://github.com/marijnz/unity-toolbar-extender.git");
+// #endif
+// #if !TOOLS_FREYA
+// 			ClientAdd("https://github.com/FreyaHolmer/Mathfs.git");
+// #endif
+// #if !TOOLS_COMP_VIS
+// 			ClientAdd("com.needle.compilation-visualizer");
+// #endif
+// #if !TOOLS_RENAMER
+// 			ClientAdd("https://github.com/redbluegames/unity-mulligan-renamer.git?path=/Assets/RedBlueGames/MulliganRenamer");
+// #endif
+// #if !TOOLS_NAUATTR
+// 			ClientAdd("com.dbrizov.naughtyattributes");
+// #endif
+// #if !TOOLS_ANIM_SEQ
+// 			ClientAdd("com.brunomikoski.animationsequencer");
+// #endif
+// #if !TOOLS_TWEEN_PLAYABLES
+// 			ClientAdd("https://github.com/AnnulusGames/TweenPlayables.git?path=/Assets/TweenPlayables");
+// #endif
+// #if !TOOLS_SG_VARS
+// 			ClientAdd("https://github.com/Cyanilux/ShaderGraphVariables.git");
+// #endif
+// 		}
 
-		private static void ClientAdd(string link)
-		{
-			Debug.LogWarning(link);
-			Client.Add(link);
-		}
-	}
-}
+// 		private static void ClientAdd(string link)
+// 		{
+// 			Debug.LogWarning(link);
+// 			Client.Add(link);
+// 		}
+// 	}
+// }
