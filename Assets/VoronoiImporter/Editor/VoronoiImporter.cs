08a5a1cd653bdf5649465de3f2d02b2c1ede2dd6
Jason Davies <arkham195@gmail.com>
2023-09-08 00:41:36 +0200

    move to separate

--- Assets/VoronoiImporter/Editor/VoronoiImporter.cs
+++ Assets/VoronoiImporter/Editor/VoronoiImporter.cs
@@ -43,38 +43,4 @@ namespace JD.VoronoiImporter
 			return texture;
 		}
 	}
-
-	public struct VoronoiInitJob : IJobFor
-	{
-		private NativeArray<Color32> pixels;
-		private readonly Color32 background;
-		private readonly int width;
-		private readonly int height;
-
-		public VoronoiInitJob(NativeArray<Color32> pixels, Color32 background, int width, int height)
-		{
-			this.pixels = pixels;
-			this.background = background;
-			this.width = width;
-			this.height = height;
-		}
-
-		public void Execute(int index)
-		{
-			int x = index / width;
-			int y = index % width;
-
-			float nx = (float)x / width;
-			float ny = (float)y / height;
-
-			if (pixels[index].EqualRGBA(background) || pixels[index].a == 0)
-			{
-				pixels[index] = Color.clear;
-			}
-			else
-			{
-				pixels[index] = new Color(nx, ny, 0, 1);
-			}
-		}
-	}
 }
