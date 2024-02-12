698c7c091be1a834ebf78d00d5dc7cd375a52a1a
JD <arkham195@gmail.com>
2024-02-12 20:24:36 +0100

    Update LocalizationExtensions.cs

--- Assets/Tools/Extensions/Runtime/LocalizationExtensions.cs
+++ Assets/Tools/Extensions/Runtime/LocalizationExtensions.cs
@@ -25,6 +25,13 @@ namespace JD
 			text.SetText(localizedString);
 		}
 
+		[Obsolete("'SetLocalizedText(TMP_Text,string,string,string)' is obsolete use 'SetLocalizedTextFormat'")]
+		[EditorBrowsable(EditorBrowsableState.Never)]
+		public static void SetLocalizedText(this TMP_Text text, string tableKey, string localeKey, string format = "{0}")
+		{
+			SetLocalizedTextFormat(text, tableKey, localeKey, format);
+		}
+
 		public static void SetLocalizedTextFormat(this TMP_Text text, string tableKey, string localeKey, string format = "{0}")
 		{
 			LocalizedStringDatabase stringDatabase = LocalizationSettings.StringDatabase;
