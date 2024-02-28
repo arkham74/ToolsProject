fb35dbe94908cd241a2a77d8545c3cfe97257c51
JD <arkham195@gmail.com>
2024-02-28 17:57:14 +0100

    flip book anim more options

--- Assets/Tools/UI/FlipBookAnimation.cs
+++ Assets/Tools/UI/FlipBookAnimation.cs
@@ -1,39 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using NaughtyAttributes;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace JD
 {
+	[ExecuteAlways]
 	public class FlipBookAnimation : MonoBehaviour
 	{
-		public Image target;
-		public float frameRate = 60;
-		public bool loop = true;
-		public List<Sprite> frames = new List<Sprite>();
+		[SerializeField] private Image target;
+		[SerializeField] private float frameRate = 60;
+		[SerializeField] private bool loop = true;
+		[SerializeField] private bool unscaledTime = false;
+		[SerializeField] private bool reverse = false;
+		[SerializeField] private List<Sprite> frames = new List<Sprite>();
 
-		private float framefloat;
+		private float framefloat = 0;
 
 		private void Reset()
 		{
 			target = GetComponentInChildren<Image>();
 		}
 
+		[Button]
+		private void Restart()
+		{
+			framefloat = 0;
+		}
+
 		private void Update()
 		{
-			framefloat += Time.deltaTime * frameRate;
-			int frame = Mathf.FloorToInt(framefloat);
-
-			if (loop)
+			if (target && frames != null && frames.Count >= 1)
 			{
-				target.sprite = frames.Repeat(frame);
+				framefloat += GetTime() * frameRate;
+				int floor = Mathf.FloorToInt(framefloat);
+				int frame = GetFrame(floor);
+				target.overrideSprite = GetSprite(frame);
 			}
 			else
 			{
-				target.sprite = frames.AtIndexClamp(frame);
+				target.overrideSprite = null;
 			}
+		}
+
+		private Sprite GetSprite(int frame)
+		{
+			return loop ? frames.Repeat(frame) : frames.AtIndexClamp(frame);
+		}
+
+		private int GetFrame(int frame)
+		{
+			int count = frames.Count - 1;
+			int reversed = count - frame;
+			return reverse ? reversed : frame;
+		}
+
+		private float GetTime()
+		{
+			return unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
 		}
 	}
 }
